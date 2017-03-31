019549bb3353296bc1be1c116d4274518e8a7034
Haozhan Huang <HHuang@Haozhans-MacBook.local>
2017-03-31 00:23:32 -0500

    Add method to set entry

--- Matrix.java
+++ Matrix.java
@@ -4,9 +4,9 @@ public class Matrix {
 	private int[][] x;
 	
 	//Constructor of the Matrix
-	public Matrix(int[][] m)
+	public Matrix(int s)
 	{
-		x = m;
+		x = new int[s][s];
 	}
 	
 	//return the size of the matrix
@@ -15,9 +15,21 @@ public class Matrix {
 		return x.length;
 	}
 	
+	public void setEntry(int r, int c,int n)
+	{
+		x[r][c] = n;
+	}
+	
 	//get the entry in the matrix
 	public int getEntry(int a, int b)
 	{
 		return x[a][b];
+	}
+	
+	public Matrix subMatrix(int r, int c)
+	{
+		sub = 
+		
+		return sub;
 	}
 }
