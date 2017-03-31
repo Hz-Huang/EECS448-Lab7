e8219a5279f85156da8342a7e2c89ce661097ac4
Haozhan Huang <HHuang@Haozhans-MacBook.local>
2017-03-31 01:00:49 -0500

    Change sub matrix method and add determinant method

--- Matrix.java
+++ Matrix.java
@@ -28,28 +28,50 @@ public class Matrix {
 		return x[a][b];
 	}
 	
+	
+	//create sub matrix of this matrix
 	public Matrix subMatrix(int r, int c)
 	{
 		Matrix sub = new Matrix(size-1);
-		int row = 0;
+		int row = 0; 
 		for (int i = 0; i < size; ++i)
 		{
-			if (i != r)
+			if (i == r) continue;
+			
+			int col = 0;
+			for (int j = 0; j < size; ++j)
 			{
-				int col = 0;
-				for (int j = 0; j < size; ++j)
-				{
-					if (j != c)
-					{
-						sub.setEntry(row, col, x[i][j]);
-					}
-					col++;
-				}
+				if (j == c) continue;
+				
+				sub.x[row][col] = x[i][j];
+				++col;
 			}
+			
 			++row;
 		}
 		
-		
 		return sub;
+	}
+	
+	//calculate determinant
+	public double determinant()
+	{
+		double det = 0;
+		if(x.length == 1)
+		{
+			det = x[0][0];
+		}
+		else if(x.length == 2)
+		{
+			det = (x[0][0] * x[1][1] - x[0][1] * x[1][0]);
+		}
+		else
+		{
+			for(int i = 0; i < size; i++)
+			{
+				det += Math.pow(-1.0, (double)i) * x[0][i] * subMatrix(0, i).determinant();
+			}
+		}
+		return det;
 	}
 }
